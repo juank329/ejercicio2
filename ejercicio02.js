e2c0f0ee4114f51bab1a43253523b46c7fe0464c
Juan Carlos Avendaño Valenzuela <109890592+juank329@users.noreply.github.com>
2022-12-12 00:28:42 -0300

    Add files via upload
    elercicio 2

---
+++ ejercicio02.js
@@ -0,0 +1,32 @@
+const miInformacion = {
+    nombre: "Juank",
+    edad: 42,
+    desarrollador: true,
+    fechaDeNacimiento: new Date("1980-05-17"),
+    libroFavorito: {
+      titulo: "el principito",
+      autor: "Antoine De Saint-exupéry",
+      fecha: new Date("1943-04-06"),
+      url: "https://www.buscalibre.cl/libro-el-principito/9789563600810/p/47278823",
+    }
+  };
+  
+  console.log(miInformacion);
+  
+  
+  
+  
+  
+
+
+//Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+// - Tu nombre (string)
+
+// - Tu edad (number)
+
+// - ¿Eres desarrollador? (boolean)
+
+// - Tu fecha de nacimiento (Date)
+
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
